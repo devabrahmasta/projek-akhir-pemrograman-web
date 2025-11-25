cb31afa9f0f88cda5f6fea624fe928b986feb4f3
Lintang <lintangwaladi@gmail.com>
2025-11-25 09:09:23 +0700

    connection

---
+++ config/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$port = "8111";
+$database = "db_power_gym";
+
+$connection = new mysqli($hostname, $username, $password, $database, $port);
+
